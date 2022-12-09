83399ee2cc00e4e9763a561ad9b8769bde1fae1e
HoriuchiAkira <horiuchiakira.mail@gmail.com>
2022-12-09 09:50:41 +0200

    add block class

--- src/utils/utils.gd
+++ src/utils/utils.gd
@@ -1,1 +1,14 @@
 extends Node
+
+enum BLOCKS {
+	EMPTY = 0,
+	SOLID = 253,
+	
+	GRASS_1 = 1,
+	GRASS_2 = 5,
+	GRASS_3 = 7,
+	
+	FLOWER_1 = 3,
+	FLOWER_2 = 4,
+	FLOWER_3 = 6
+}
--- src/utils/world_gen.gd
+++ src/utils/world_gen.gd
@@ -3,15 +3,21 @@ extends Node
 const size = 1000
 
 class Block:
-	var type: int
+	var biome: int
 	var obstacle: int = 0
+	var custom_obstacle_color = null
+	var custom_color = null
 	var walkable: bool
+#	var rotation: int
+	var v_flip: bool = false
+	var h_flip: bool = false
+	
 
 func _ready():
 	pass
 
 
-enum BLOCK_TYPE {
+enum BIOME_TYPE {
 	NORMAL,
 	DESERT,
 	CORRUPTION
